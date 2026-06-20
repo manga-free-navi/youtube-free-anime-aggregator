57ab0547b5810878ba78cbd9eeee5a4544673454
amtokyo713 <amtokyo713@gmail.com>
2026-06-20 15:45:41 +0900

    fix: resolve PWA paths and cache registration errors

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'anime-free-cache-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/manifest.json',
-  '/icon.svg'
+  './',
+  'manifest.json',
+  'icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
